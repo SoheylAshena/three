37367a57ac400694810be006ab042bba1533500b
Soheyl Ashena <sashena203@gmail.com>
2025-10-09 22:37:49 +0330

    Enhanced UI

--- src/sections/homeSection.ts
+++ src/sections/homeSection.ts
@@ -1,12 +1,32 @@
 export function renderHomeSection() {
-  const h1 = document.createElement("h1");
-  h1.textContent = "Hello";
+  const elements = [];
 
-  const p1 = document.createElement("p");
-  p1.textContent = "Welcome to my website";
+  const hello = document.createElement("p");
+  hello.classList.add("hello-text");
+  hello.textContent = "Hello, Traveler.";
+  elements.push(hello);
+
+  const welcome = document.createElement("p");
+  welcome.classList.add("welcome-text");
+  welcome.textContent = "Welcome to the Orbit of Soheyl.";
+  elements.push(welcome);
+
+  const introduction = document.createElement("p");
+  introduction.classList.add("introduction-text");
+  introduction.textContent = "I am Soheyl,";
+  elements.push(introduction);
+
+  const career = document.createElement("p");
+  career.classList.add("career-text");
+  career.textContent = "A Creative Front-End Developer Exploring the Cosmos.";
+  elements.push(career);
 
   const div = document.createElement("div");
-  div.appendChild(h1);
-  div.appendChild(p1);
+  div.classList.add("home-page");
+
+  elements.forEach((item) => {
+    div.appendChild(item);
+  });
+
   return div;
 }
